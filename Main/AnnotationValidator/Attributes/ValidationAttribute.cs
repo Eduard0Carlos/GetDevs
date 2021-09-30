c8ac8a5313a9694ac5d1aa26cdf98506af5c0f40
Matheus Marciano <marciannoooo0@gmail.com>
2021-09-30 14:20:26 -0300

    teste

---
+++ Main/AnnotationValidator/Attributes/ValidationAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogical.Attributes
+{
+    [System.AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    sealed class ValidationAttribute : Attribute
+    {
+        public string MethodName { get; set; }
+        public Type ValidationClass { get; set; }
+        public int MaxLenght { get; set; } = 100;
+        public int MinLenght { get; set; }
+        public bool HasNormalize { get; set; }
+        public bool HasHash { get; set; }
+        public bool IsRequired { get; set; }
+        public bool IsEmail { get; set; }
+        public bool IsCPF { get; set; }
+        public bool IsTelefone { get; set; }
+        public bool IsFixedLength { get; set; }
+        public bool LettersOnly { get; set; }
+        public int FixedLength { get; set; }
+        public MethodInfo ValidationMehtod
+        {
+            get
+            {
+                return this.ValidationClass?.GetMethod(this.MethodName);
+            }
+        }
+        public ValidationAttribute()
+        {
+        }
+        public ValidationAttribute(string validationMethodName, Type validationClass)
+        {
+            this.MethodName = validationMethodName;
+            this.ValidationClass = validationClass;
+        }
+    }
+}
