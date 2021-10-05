00883b6f1b1fb18631641bad219ba6ac61ac8e8b
Eduard0Carlos <carloseduardo.blu@outlook.com>
2021-10-05 16:11:40 -0300

    added AIContext

---
+++ Main/WebRankingML/ReusmeResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebRankingML
+{
+    public class ReusmeResult
+    {
+        public uint GroupId { get; set; }
+        public uint Label { get; set; }
+        public float Score { get; set; }
+        public float[] Features { get; set; }
+
+    }
+}
