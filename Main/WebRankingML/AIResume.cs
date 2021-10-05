57939b5a4ad38ec214f45b0d38087a469333260d
Marciann0 <marciannoooo0@outlook.com>
2021-10-05 15:00:20 -0300

    added WebRankingML project

--- Main/WebRankingML/AIResume.cs
+++ Main/WebRankingML/AIResume.cs
@@ -1,5 +1,4 @@
-﻿using Domain.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebRankingML
 {
-    public class AIResume : EntityBase
+    public class AIResume
     {
         public int GroupId { get; protected set; }
         public float Skills { get; protected set; }
