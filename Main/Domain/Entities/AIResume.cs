c117ac59d04139d5f9c2510b691ab25bb7a43db6
NicolasDestito <erickyuridossantos2005@gmail.com>
2021-10-05 14:54:54 -0300

    added WebRankingML project

---
+++ Main/Domain/Entities/AIResume.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class AIResume : EntityBase
+    {
+        public int GroupId{ get; protected set; }
+        public float Skills { get; protected set; }
+        public float Educations { get; protected set; }
+        public float Idioms { get; protected set; }
+        public float BusinessBonds { get; protected set; }
+    }
+}
---
+++ Main/WebRankingML/AIResume.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebRankingML
+{
+    public class AIResume : EntityBase
+    {
+        public int GroupId { get; protected set; }
+        public float Skills { get; protected set; }
+        public float Educations { get; protected set; }
+        public float Idioms { get; protected set; }
+        public float BusinessBonds { get; protected set; }
+    }
+}
