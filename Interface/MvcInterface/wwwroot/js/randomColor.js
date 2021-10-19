86fd6aad3eeaf2835e6378b0789e88b8ec37aa2b
Carlos Eduardo Vieira <carloseduardo.blu@outlook.com>
2021-10-19 09:29:16 -0300

    added profile page

---
+++ Interface/MvcInterface/wwwroot/js/randomColor.js
@@ -0,0 +1,14 @@
+﻿function getRandomColor() {
+        var x = Math.floor(Math.random() * 36);
+        var y = 5 + Math.floor(Math.random() * 36);
+        var z = 10 + Math.floor(Math.random() * 36);
+        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        return bgColor;
+}
+
+function setRandomColor() {
+    $("#banner").css("background-color", getRandomColor());
+    $("body").css("background-color", "#F3F2EF");
+}
+
+setRandomColor();
